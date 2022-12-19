66c065ad603928cdec2c95b449f9fb87f9fe71bd
Ivan Carlos <ivancarlos50@gmail.com>
2022-12-19 10:44:59 -0300

    Commit Final

--- src/containers/Player.tsx
+++ src/containers/Player.tsx
@@ -1,6 +1,6 @@
 import {headerInfo, tracks} from '../services/api';
 import PlaylistHeader from "./components/PlaylistHeader";
-/* import PlaylistItem from "./components/PlaylistItem"; */
+import PlaylistItem from "./components/PlaylistItem"; 
 
 const Player = () => {
 
@@ -8,10 +8,10 @@ const Player = () => {
        
        <div className="player-container">
         <PlaylistHeader { ...headerInfo} />
-        {/* {
-            tracks.map((track) => (
-                <PlaylistItem {...track}/>
-                ))} */}
+            { 
+                tracks.map((track) => (
+                    <PlaylistItem {...track} key={track.id}/>
+            ))} 
        </div>
     );
 };
--- src/containers/components/PlaylistItem.tsx
+++ src/containers/components/PlaylistItem.tsx
@@ -1,10 +1,35 @@
-const PlaylistItem = () => {
+import {useState} from 'react';
+import {liked, noliked} from '../../assets/images/buttons';
+
+
+type TrackProps = {
+    title:string;
+    like: boolean;
+    albumName: string;
+    cover: string;
+};
+
+const PlaylistItem = ({title, like, cover, albumName }: TrackProps) => {
+    
+    const [isLiked, setIsLiked] = useState(like);
+
+    const handleLike = ()=>{
+        setIsLiked(!isLiked);
+    }
 
     return (
-        <div>
-            <h3>PlaylistItem</h3>
+        <div className="music-container">
+            <div className="music-content">
+                <img src={cover} alt={title} className="music-cover" />
+                <div className="music-name">
+                    <h4>{title}</h4>&nbsp;-&nbsp;<h6>{albumName}</h6>
+                </div>
+                <button onClick={()=> handleLike()} className="button-like">
+                    <img src={isLiked ? liked : noliked} alt="Like" className="like" />
+                </button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default PlaylistItem;
